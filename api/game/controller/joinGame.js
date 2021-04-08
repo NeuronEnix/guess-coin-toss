9a45d68348a86e8aa8f68c744bc426fee4498cca
NeuronEnix <kaushikrb909@gmail.com>
2021-04-08 19:04:44 +0530

    Added: join game

--- api/game/controller/joinGame.js
+++ api/game/controller/joinGame.js
@@ -1,13 +1,14 @@
 const moment = require('moment');
 const { resOk, resErr, resErrType, resRender } = require( "../../../handler").resHandler;
-const { gameIndex, userSocketIndex } = require( "../sok/playGame" );
+const { gameIndex, userSocketIndex, userGameIndex } = require( "../sok/playGame" );
 module.exports = async function hostGame( req, res, next ) {    
 
     try {
-        
-        return resRender( res, "playGamePage", {
-            
-        } );
+        const { game_id } = req.query;
+        const { uid, name } = req.session;
+        userGameIndex[ uid ] = game_id;
+        gameIndex[ game_id ].plr2 = uid;
+        return res.redirect( "/game/play" );
         
     } catch ( err ) {
         return resErr( res, resErrType.unknownErr, { infoToServer:err } );
--- api/game/router.js
+++ api/game/router.js
@@ -3,7 +3,9 @@ const { isAuthenticated } = require( "../../handler").sessionHandler;
 // const { resRender } = require( "../../handler" ).resHandler;
 
 const playGame = require( "./controller/playGame" );
+const joinGame = require( "./controller/joinGame" );
 
 router.get( "/play", isAuthenticated, playGame );
+router.get( "/join", isAuthenticated, joinGame );
 
 module.exports = router;
